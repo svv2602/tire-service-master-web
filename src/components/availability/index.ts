2b638dc1c3be31a5772325f21dc210c9858f4225
svv2602 <svv2602@gmail.com>
2025-06-06 13:18:36 +0300

    chore: remove old availability components after migration to centralized styling system

--- src/components/availability/index.ts
+++
@@ -1,7 +0,0 @@
-// components/availability/index.ts
-// Экспорт всех компонентов системы доступности
-
-export { AvailabilityCalendar } from './AvailabilityCalendar';
-export { TimeSlotPicker } from './TimeSlotPicker';
-export { DayDetailsPanel } from './DayDetailsPanel';
-export { AvailabilitySelector } from './AvailabilitySelector'; 
