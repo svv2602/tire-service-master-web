9cfde06a7f9b4bfed003fb1c40753949c1cc354d
svv2602 <svv2602@gmail.com>
2025-06-25 11:51:07 +0300

    Исправлена страница CitiesPage\n\nУдалена избыточная логика редактирования городов,\nтак как CitiesList уже имеет встроенную функциональность\nредактирования через модальное окно.

--- src/pages/cities/CitiesPage.tsx
+++ src/pages/cities/CitiesPage.tsx
@@ -1,30 +1,19 @@
 import React from 'react';
-import { Box, Typography, IconButton, Tooltip } from '@mui/material';
-import { Edit as EditIcon } from '@mui/icons-material';
-import { useNavigate } from 'react-router-dom';
-import { useTheme } from '@mui/material/styles';
+import { Box, Typography } from '@mui/material';
 import { SIZES } from '../../styles/theme';
 import CitiesList from '../../components/CitiesList';
-import { City } from '../../types/models';
 
 const CitiesPage: React.FC = () => {
-  const navigate = useNavigate();
-  const theme = useTheme();
-
   // Временное решение - захардкоженный ID региона
   // TODO: Получать ID региона из URL или состояния приложения
   const defaultRegionId = 1;
-
-  const handleEditCity = (city: City) => {
-    navigate(`/admin/cities/${city.id}/edit`);
-  };
 
   return (
     <Box>
       <Typography variant="h4" sx={{ mb: SIZES.spacing.lg }}>
         Управление городами
       </Typography>
-      <CitiesList regionId={defaultRegionId} onEditCity={handleEditCity} />
+      <CitiesList regionId={defaultRegionId} />
     </Box>
   );
 };
