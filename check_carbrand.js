b530da2e594559e075e5db838fe3ea41cb9b16b4
svv2602 <svv2602@gmail.com>
2025-07-15 12:24:15 +0300

    Чистка корня и external-files: удалены временные, тестовые, промежуточные и устаревшие файлы, оставлены только чеклисты, правила и необходимые конфиги. Обновлён README.md с навигацией.

--- check_carbrand.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const ru = JSON.parse(fs.readFileSync('src/i18n/locales/ru.json', 'utf8'));
-console.log('carBrand в основной секции forms:', !!ru.forms.carBrand);
-console.log('carBrand в forms.forms:', !!ru.forms.forms.carBrand);
